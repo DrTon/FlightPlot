38b99a39d80c04ce6509ee38b74f4a08869bafe7
Beat Küng <beat-kueng@gmx.net>
2016-05-20 13:12:45 +0200

    FieldsListDialog: add field on double click

--- src/me/drton/flightplot/FieldsListDialog.java
+++ src/me/drton/flightplot/FieldsListDialog.java
@@ -1,9 +1,24 @@
 package me.drton.flightplot;
 
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.KeyStroke;
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.*;
-import java.util.*;
 
 public class FieldsListDialog extends JDialog {
     private JPanel contentPane;
@@ -41,6 +56,14 @@ public class FieldsListDialog extends JDialog {
                 onClose();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        fieldsTable.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                if (me.getClickCount() == 2) {
+                    callbackAdd.run();
+                }
+            }
+        });
     }
 
     private void onClose() {
