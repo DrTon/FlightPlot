1aaa18e0043e354d95add601d889664686209356
Anton Babushkin <anton.babushkin@me.com>
2017-04-22 18:32:44 +0200

    Version updated: 0.2.25

--- src/me/drton/flightplot/FlightPlot.java
+++ src/me/drton/flightplot/FlightPlot.java
@@ -73,7 +73,7 @@ public class FlightPlot {
     }
 
     private static String appName = "FlightPlot";
-    private static String version = "0.2.24";
+    private static String version = "0.2.25";
     private static String appNameAndVersion = appName + " v." + version;
     private static String colorParamPrefix = "Color ";
     private final Preferences preferences;
